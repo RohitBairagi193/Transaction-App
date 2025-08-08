baca4368d106c36287c1ccbad892c8ea7b8d67fe
RohitBairagi193 <149870511+RohitBairagi193@users.noreply.github.com>
2025-08-08 17:33:53 +0530

    Update Database.js

--- src/Modules/Database.js
+++ src/Modules/Database.js
@@ -5,12 +5,11 @@ const app = express();
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host: process.env.host,
-    user: process.env.user,
-    password: process.env.password,
-    database: process.env.dbname,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
-
 db.connect((err) => {
     if (err) {
         console.error("Error connecting to database:", err);
